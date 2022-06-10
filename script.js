5ea24187955cbc7b40d75c4a639560a26e3d63f0
Jason <Jason.Wong47@myhunter.cuny.edu>
2022-06-10 19:55:45 -0400

    Added the ability to add columns to the table and movedd menu bar to the right side

--- script.js
+++ script.js
@@ -2,16 +2,45 @@ let addRowBtn = document.getElementById("add-row");
 let addColBtn = document.getElementById("add-column");
 let removeRowBtn = document.getElementById("remove-row");
 let removeColBtn = document.getElementById("remove-column");
-let table = document.getElementById("grid");
+let grid = document.getElementById("grid-container");
 
 addRowBtn.addEventListener("click", addRow);
+addColBtn.addEventListener("click", addCol);
+removeRowBtn.addEventListener("click", removeRow);
+removeColBtn.addEventListener("click", removeCol);
+
+let rowCount = 0;
 
 function addRow()
 {
-    let row = table.insertRow(table.rows.length);
-    for (let i = 0; i < table.rows[0].cells.length; i++)
+    if(document.querySelectorAll(".row").length === 0)
     {
-        let cell = row.insertCell(i);
-        cell.innerHTML = i;
+        let gridRow = document.createElement("div");
+        gridRow.classList.add("row");
+    }
+    let gridRow = document.createElement("div");
+    gridRow.classList.add("row");
+    gridRow.id = "row" + rowCount;
+    let gridSquare = document.createElement("div");
+    gridSquare.classList.add("grid-square");
+    gridRow.appendChild(gridSquare);
+    grid.appendChild(gridRow);
+    rowCount++;
+}
+
+function addCol()
+{
+    if(rowCount === 0)
+    {
+        addRow();
+    }
+    else
+    {
+        let getRows = document.querySelectorAll(".row");
+        getRows.forEach(function(row){
+            let gridSquare = document.createElement("div");
+            gridSquare.classList.add("grid-square");
+            row.appendChild(gridSquare);
+        })
     }
 }
