94fdd159bf329b045d8ca5bccd7153237a3ebf8a
Henry <aosposx@gmail.com>
2022-06-11 01:53:26 -0400

    fixed grid add col and row issue

--- script.js
+++ script.js
@@ -9,51 +9,52 @@ addColBtn.addEventListener("click", addCol);
 removeRowBtn.addEventListener("click", removeRow);
 removeColBtn.addEventListener("click", removeCol);
 
-let rowCount = 0;
+let rowIndex = 0;
+let colIndex = 0;
 
-function addRow()
-{
-    if(document.querySelectorAll(".row").length === 0)
-    {
-        let gridRow = document.createElement("div");
-        gridRow.classList.add("row");
-    }
-    let gridRow = document.createElement("div");
-    gridRow.classList.add("row");
-    gridRow.id = "row" + rowCount;
-    let gridSquare = document.createElement("div");
-    gridSquare.classList.add("grid-square");
-    gridRow.appendChild(gridSquare);
-    grid.appendChild(gridRow);
-    rowCount++;
+function addRow() {
+  let gridRow = document.createElement("div");
+  gridRow.classList.add("row");
+  gridRow.id = "row" + rowIndex;
+  grid.appendChild(gridRow);
+  for (let i = 0; i <= colIndex; i++) {
+    addSquare(gridRow.id);
+  }
+  rowIndex++;
 }
 
-function addCol()
-{
-    if(rowCount === 0)
-    {
-        addRow();
-    }
-    else
-    {
-        let getRows = document.querySelectorAll(".row");
-        getRows.forEach(function(row){
-            let gridSquare = document.createElement("div");
-            gridSquare.classList.add("grid-square");
-            row.appendChild(gridSquare);
-        })
-    }
-}
-
-function removeRow()
-{
-    grid.lastChild.remove();
-}
-
-function removeCol()
-{
+function addCol() {
+  if (rowIndex === 0) {
+    addRow();
+  } else {
     let getRows = document.querySelectorAll(".row");
-    getRows.forEach(function(row){
-        row.lastChild.remove();
-    })
+    getRows.forEach(function (row) {
+      addSquare(row.id);
+    });
+    colIndex++;
+  }
+}
+
+function addSquare(rowId) {
+  let gridRow = document.querySelector(`#${rowId}`);
+  let gridSquare = document.createElement("div");
+  gridSquare.classList.add("grid-square");
+  gridRow.appendChild(gridSquare);
+}
+
+function removeRow() {
+  if (rowIndex > 0) {
+    grid.lastChild.remove();
+    rowIndex--;
+  }
+}
+
+function removeCol() {
+  if (colIndex > 0) {
+    let getRows = document.querySelectorAll(".row");
+    getRows.forEach(function (row) {
+      row.lastChild.remove();
+    });
+    colIndex--;
+  }
 }
