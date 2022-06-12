290d27d538fe9ba02e7e5482fd3ca3a5a4515a44
Jason <Jason.Wong47@myhunter.cuny.edu>
2022-06-12 12:59:54 -0400

    Added 'fill-grid-function' to the list of functions.

--- script.js
+++ script.js
@@ -2,12 +2,20 @@ let addRowBtn = document.getElementById("add-row");
 let addColBtn = document.getElementById("add-column");
 let removeRowBtn = document.getElementById("remove-row");
 let removeColBtn = document.getElementById("remove-column");
-let grid = document.getElementById("grid-container");
+const grid = document.getElementById("grid-container");
+
+let colorPicker = document.getElementById("color-picker");
+let fill = document.getElementById("fill-grid");
+let fillUncolored = document.getElementById("fill-uncolored");
+let clearGridBtn = document.getElementById("clear-grid");
 
 addRowBtn.addEventListener("click", addRow);
 addColBtn.addEventListener("click", addCol);
 removeRowBtn.addEventListener("click", removeRow);
 removeColBtn.addEventListener("click", removeCol);
+fill.addEventListener("click", fillGrid);
+fillUncolored.addEventListener("click", fillUncoloredGrid);
+clearGridBtn.addEventListener("click", clearGrid);
 
 let rowIndex = 0;
 let colIndex = 0;
@@ -25,7 +33,6 @@ function addRow() {
   for (let i = 0; i < colIndex; i++) {
     addSquare(gridRow.id);
   }
-
   rowIndex++;
 }
 
@@ -77,4 +84,25 @@ function removeAllGridChildren() {
   grid.replaceChildren();
   colIndex = 0;
   rowIndex = 0;
+}
+
+function fillGrid()
+{
+  let getRows = document.querySelectorAll(".row");
+  getRows.forEach(function (row) {
+    let getSquares = document.querySelectorAll(".grid-square");
+    getSquares.forEach(function (square) {
+      square.style.backgroundColor = colorPicker.value;
+    });
+  });
+}
+
+function fillUncoloredGrid()
+{
+
+}
+
+function clearGrid()
+{
+  
 }
